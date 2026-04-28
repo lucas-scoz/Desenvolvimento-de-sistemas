16c29e74a1dcf1382f8d6b1df65e44c6ca1f4917
lucascenci <lucascenciscoz@gmail.com>
2026-04-27 21:43:39 -0300

    crud

--- BD/usuarios/server.js
+++ BD/usuarios/server.js
@@ -19,20 +19,91 @@ app.get('/usuario/:id', async (req, res) => {
   const id = parseInt(req.params.id);
 
   if(isNaN(id)){
-    res.status(400).send("o id e invalido")
+    return res.status(400).send("o id e invalido")
   }
 
   try{
       const [rows] = await db.query("Select * from usuario where id = ?" , [id])
-      if(rows.lenght() == 0){
+      if(rows.length == 0){
         return res.status(404).send("usuario nao encontrado")
       }
       res.json(rows)
   }catch(e){
+    console.log(e)
     res.status(500).send("erro ao buscar tarefa!")
   }
 })
+app.post('/usuario',async (req , res) => {
+  let {nome , idade} = req.body;
 
+  if(!nome || !idade){
+      res.status(400).send("Nome ou idade devem ser preenchidos")
+  }
+
+  try{
+   const [result] =await db.query("INSERT INTO usuario (nome,idade) values (? ,?)", [nome , idade])
+   // result -> retorna o id(insertId)
+   const novoUsuario = {id: result.insertId, nome, idade}
+   res.status(201).json(novoUsuario)
+  }catch(e){
+    console.log(e)
+    res.status(500).send("erro ao criar usuario!")
+  }
+  res.send('POST request to the homepage')
+
+} )
+
+app.put("/usuario/:id" , async (req , res) => {
+ const id = parseInt(req.params.id)
+ let{nome , idade} = req.body;
+
+ 
+  if(isNaN(id)){
+    return res.status(400).send("o id e invalido")
+  }
+ if(!nome || !idade){
+      return res.status(400).send("Nome ou idade devem ser preenchidos")
+  }
+   try{
+      const [rows] = await db.query("Select * from usuario where id = ?" , [id])
+      if(rows.length == 0){
+        return res.status(404).send("usuario nao encontrado")
+      }
+
+      const[result] =await db.query("update usuario set nome = ?, idade= ?  where id = ?", [nome ,idade , id])
+      res.status(201).send("usuario atualizado com sucesso")
+
+      if (result.affectedRows == 0){
+        return res.status(400).send("nenhum dado foi alterado")
+      }
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro ao atualizar usuario!")
+    }
+
+})
+
+app.delete("/usuario/:id" , async(req , res) => {
+  const id = parseInt(req.params.id)
+  if(isNaN(id)){
+  return res.status(400).send("o id e invalido")
+}
+try{
+  const [rows] = await db.query("select * from usuario where id = ?", [id])
+  if(rows.length == 0){
+    return res.status(404).send("usuario nao encontrado!")
+  }
+  const [result] = await db.query("delete from usuario where id = ?", [id])
+
+  if(result.affectedRows == 0){
+    return result.status(400).send("nenhum dado foi excluido")
+  }
+    res.status(204).send("o usuario foi excluido com sucesso")
+}catch(e){
+  console.log(e)
+  res.status(500).send("erro ao deletar usuario")
+}
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
